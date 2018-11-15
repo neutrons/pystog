89af1ab4d449599f96161406676a33677a62149d
McDonnell, Marshall <mcdonnellmtornl.gov>
2018-11-15 05:51:50 -0500

    Generate test files script

--- tests/create_test_files.py
+++ tests/create_test_files.py
@@ -1,18 +1,16 @@
 
 import numpy as np
-from tests.utils import create_and_write_both_type_of_functions
+from tests.utils import create_and_write_both_type_of_functions, nickel_kwargs, argon_kwargs
 
 def create_nickel():
-    kwargs = { "rho" : 0.0913841384754395, "<b_coh>^2" : 106.09, "<b_tot^2>" : 147.22}
     create_and_write_both_type_of_functions("nickel.gr",
                                             "nickel.real_space.dat",
-                                            "nickel.reciprocal_space.dat",**kwargs)
+                                            "nickel.reciprocal_space.dat",**nickel_kwargs)
 
 def create_argon():
-    kwargs = { "rho" : 0.02138, "<b_coh>^2" : 3.644, "<b_tot^2>" : 5.435}
     create_and_write_both_type_of_functions("argon.gr",
                                             "argon.real_space.dat",
-                                            "argon.reciprocal_space.dat",**kwargs)
+                                            "argon.reciprocal_space.dat",**argon_kwargs)
 
 
 if __name__ == "__main__":
