6e6c0cd97716fb789fe3b1392ebb42b6528c4b49
McDonnell, Marshall <mcdonnellmt@ornl.gov>
2018-12-04 01:15:45 -0500

    Simplified StoG test

--- tests/test_stog.py
+++ tests/test_stog.py
@@ -1,44 +1,10 @@
 import unittest
-from utils import \
-    load_test_data, get_index_of_function, \
-    REAL_HEADERS, RECIPROCAL_HEADERS
-from materials import Nickel, Argon
 from pystog.stog import StoG
 
 # Real Space Function
 
 
 class TestStogBase(unittest.TestCase):
-    rtol = 0.2
-    atol = 0.2
-
-    def initialize_material(self):
-        # setup input data
-        self.kwargs = self.material.kwargs
-
-        # setup the first, last indices
-        self.real_space_first = self.material.real_space_first
-        self.real_space_last = self.material.real_space_last
-
-        data = load_test_data(self.material.real_space_filename)
-        self.r = data[:, get_index_of_function("r", REAL_HEADERS)]
-        self.gofr = data[:, get_index_of_function("g(r)", REAL_HEADERS)]
-        self.GofR = data[:, get_index_of_function("G(r)", REAL_HEADERS)]
-        self.GKofR = data[:, get_index_of_function("GK(r)", REAL_HEADERS)]
-
-        # targets for 1st peaks
-        self.gofr_ff_target = self.material.gofr_ff_target
-        self.GofR_ff_target = self.material.GofR_ff_target
-        self.GKofR_ff_target = self.material.GKofR_ff_target
-
-        data = load_test_data(self.material.reciprocal_space_filename)
-        self.q = data[:, get_index_of_function("Q", RECIPROCAL_HEADERS)]
-        self.sq = data[:, get_index_of_function("S(Q)", RECIPROCAL_HEADERS)]
-        self.fq = data[:, get_index_of_function("F(Q)", RECIPROCAL_HEADERS)]
-        self.fq_keen = data[:, get_index_of_function(
-            "FK(Q)", RECIPROCAL_HEADERS)]
-        self.dcs = data[:, get_index_of_function("DCS(Q)", RECIPROCAL_HEADERS)]
-
     def setUp(self):
         unittest.TestCase.setUp(self)
         self.stog = StoG()
@@ -79,17 +45,3 @@ class TestStogInit(TestStogBase):
         self.assertTrue(self.stog.df_sq_master.empty)
         self.assertTrue(self.stog.df_sq_individuals.empty)
         self.assertTrue(self.stog.df_gr_master.empty)
-
-
-class TestStogNickel(TestStogBase):
-    def setUp(self):
-        super(TestStogNickel, self).setUp()
-        self.material = Nickel()
-        self.initialize_material()
-
-
-class TestStogArgon(TestStogBase):
-    def setUp(self):
-        super(TestStogArgon, self).setUp()
-        self.material = Argon()
-        self.initialize_material()
