1bdccda8aa60d7f61454f3f178bb85cfeab476a8
mdoucet <matd10@yahoo.com>
2019-01-31 12:30:24 -0500

    Update docs

--- pystog/transformer.py
+++ pystog/transformer.py
@@ -30,9 +30,9 @@ class Transformer:
     >>> transformer = Transformer()
     >>> q, sq = numpy.loadtxt("my_sofq_file.txt",unpack=True)
     >>> r = numpy.linspace(0., 25., 2500)
-    >>> r, gr = transformer.S_to_G(q, sq, r)
+    >>> r, gr, dgr = transformer.S_to_G(q, sq, r)
     >>> q = numpy.linspace(0., 25., 2500)
-    >>> q, sq = transformer.G_to_S(r, gr, q)
+    >>> q, sq, dsq = transformer.G_to_S(r, gr, q)
     """
 
     def __init__(self):
@@ -126,14 +126,14 @@ class Transformer:
         :param dy: uncertainty vector
         :type dy: numpy.array or list
         :return: vector pair (x,y) with cropping applied
-        :rtype: numpy.array pair
+        :rtype: (numpy.array, numpy.array, numpy.array)
         """
         indices = np.logical_and(x >= xmin, x <= xmax)
         y = y[indices]
         if dy is not None:
             err = np.asarray(dy)[indices]
         else:
-            err = np.zeros(y.shape)
+            err = np.zeros_like(y)
         x = x[indices]
         return x, y, err
 
@@ -153,8 +153,10 @@ class Transformer:
         :type xmin: float
         :param xmax: maximum x-value for crop
         :type xmax: float
-        :return: vector pair of transformed domain, range vectors
-        :rtype: numpy.array
+        :param dyin: uncertainty vector for yin
+        :type dyin: numpy.array or list
+        :return: vector pair of transformed domain, range vectors, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
 
         if xmax is None:
@@ -200,9 +202,11 @@ class Transformer:
         :type fq: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dfq: uncertainty on :math:`Q[S(Q)-1]`
+        :type dfq: numpy.array or list
 
-        :return: :math:`r` and :math:`G_{PDFFIT}(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`G_{PDFFIT}(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         r, gr, dgr = self.fourier_transform(q, fq, r, dyin=dfq, **kwargs)
         gr *= 2. / np.pi
@@ -219,9 +223,11 @@ class Transformer:
         :type fq: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dfq: uncertainty on :math:`Q[S(Q)-1]`
+        :type dfq: numpy.array or list
 
-        :return: :math:`r` and :math:`G_{Keen Version}(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`G_{Keen Version}(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         r, gr, dgr = self.F_to_G(q, fq, r, dfq, **kwargs)
         gr, dgr = self.converter.G_to_GK(r, gr, dgr, **kwargs)
@@ -237,9 +243,11 @@ class Transformer:
         :type fq: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dfq: uncertainty on :math:`Q[S(Q)-1]`
+        :type dfq: numpy.array or list
 
-        :return: :math:`r` and :math:`g(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`g(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         r, gr, dgr = self.F_to_G(q, fq, r, dfq, **kwargs)
         gr, dgr = self.converter.G_to_g(r, gr, dgr, **kwargs)
@@ -256,9 +264,11 @@ class Transformer:
         :type sq: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dsq: :math:`S(Q)` uncertainties
+        :type dsq: numpy.array or list
 
-        :return: :math:`r` and :math:`G_{PDFFIT}(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`G_{PDFFIT}(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.S_to_F(q, sq, dsq)
         return self.F_to_G(q, fq, r, dfq, **kwargs)
@@ -273,9 +283,11 @@ class Transformer:
         :type sq: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dsq: :math:`S(Q)` uncertainties
+        :type dsq: numpy.array or list
 
-        :return: :math:`r` and :math:`G_{Keen Version}(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`G_{Keen Version}(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.S_to_F(q, sq, dsq)
         return self.F_to_GK(q, fq, r, dfq, **kwargs)
@@ -290,9 +302,11 @@ class Transformer:
         :type sq: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dsq: :math:`S(Q)` uncertainties
+        :type dsq: numpy.array or list
 
-        :return: :math:`r` and :math:`g(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`g(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.S_to_F(q, sq, dsq)
         return self.F_to_g(q, fq, r, dfq, **kwargs)
@@ -304,13 +318,15 @@ class Transformer:
 
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
-        :param fq: :math:`F(Q)` vector
-        :type fq: numpy.array or list
+        :param fq_keen: :math:`F(Q)` vector
+        :type fq_keen: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dfq_keen: :math:`F(Q)` vector uncertainties
+        :type dfq_keen: numpy.array or list
 
-        :return: :math:`r` and :math:`G_{PDFFIT}(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`G_{PDFFIT}(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.FK_to_F(q, fq_keen, dfq_keen, **kwargs)
         return self.F_to_G(q, fq, r, dfq, **kwargs)
@@ -321,13 +337,15 @@ class Transformer:
 
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
-        :param fq: :math:`F(Q)` vector
-        :type fq: numpy.array or list
+        :param fq_keen: :math:`F(Q)` vector
+        :type fq_keen: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dfq_keen: :math:`F(Q)` vector uncertainties
+        :type dfq_keen: numpy.array or list
 
-        :return: :math:`r` and :math:`G_{Keen Version}(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`G_{Keen Version}(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.FK_to_F(q, fq_keen, dfq_keen, **kwargs)
         return self.F_to_GK(q, fq, r, dfq, **kwargs)
@@ -338,13 +356,15 @@ class Transformer:
 
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
-        :param fq: :math:`F(Q)` vector
-        :type fq: numpy.array or list
+        :param fq_keen: :math:`F(Q)` vector
+        :type fq_keen: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param dfq_keen: :math:`F(Q)` vector uncertainties
+        :type dfq_keen: numpy.array or list
 
-        :return: :math:`r` and :math:`g(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`g(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.FK_to_F(q, fq_keen, dfq_keen, **kwargs)
         return self.F_to_g(q, fq, r, dfq, **kwargs)
@@ -361,9 +381,11 @@ class Transformer:
         :type dcs: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param ddcs: :math:`\\frac{d \\sigma}{d \\Omega}(Q)` uncertainties
+        :type ddcs: numpy.array or list
 
-        :return: :math:`r` and :math:`G_{PDFFIT}(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`G_{PDFFIT}(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.DCS_to_F(q, dcs, ddcs, **kwargs)
         return self.F_to_G(q, fq, r, dfq, **kwargs)
@@ -379,9 +401,11 @@ class Transformer:
         :type dcs: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param ddcs: :math:`\\frac{d \\sigma}{d \\Omega}(Q)` uncertainties
+        :type ddcs: numpy.array or list
 
-        :return: :math:`r` and :math:`G_{Keen Version}(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`G_{Keen Version}(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.DCS_to_F(q, dcs, ddcs, **kwargs)
         return self.F_to_GK(q, fq, r, dfq, **kwargs)
@@ -397,9 +421,11 @@ class Transformer:
         :type dcs: numpy.array or list
         :param r: :math:`r`-space vector
         :type r: numpy.array or list
+        :param ddcs: :math:`\\frac{d \\sigma}{d \\Omega}(Q)` uncertainties
+        :type ddcs: numpy.array or list
 
-        :return: :math:`r` and :math:`g(r)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`r`, :math:`g(r)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         fq, dfq = self.converter.DCS_to_F(q, dcs, ddcs, **kwargs)
         return self.F_to_g(q, fq, r, dfq, **kwargs)
@@ -417,11 +443,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
-        :param dgr: uncertainty vector
+        :param dgr: :math:`G_{PDFFIT}(r)` uncertainties
         :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`Q[S(Q)-1]` vector list
-        :rtype: numpy.array list
+        :return: :math:`Q`, :math:`Q[S(Q)-1]`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         return self.fourier_transform(r, gr, q, dyin=dgr, **kwargs)
 
@@ -435,9 +461,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`G_{PDFFIT}(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`S(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`S(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         q, fq, dfq = self.G_to_F(r, gr, q, dgr=dgr, **kwargs)
         sq, dsq = self.converter.F_to_S(q, fq, dfq=dfq)
@@ -453,9 +481,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`G_{PDFFIT}(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`F(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`F(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         q, fq, dfq = self.G_to_F(r, gr, q, dgr=dgr, **kwargs)
         fq, dfq = self.converter.F_to_FK(q, fq, dfq=dfq, **kwargs)
@@ -472,9 +502,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`G_{PDFFIT}(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`\\frac{d \\sigma}{d \\Omega}(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`\\frac{d \\sigma}{d \\Omega}(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         q, fq, dfq = self.G_to_F(r, gr, q, dgr=dgr, **kwargs)
         dcs, ddcs = self.converter.F_to_DCS(q, fq, dfq=dfq, **kwargs)
@@ -491,9 +523,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`G_{Keen Version}(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`Q[S(Q)-1]` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`Q[S(Q)-1]`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         _gr, _dgr = self.converter.GK_to_G(r, gr, dgr=dgr, **kwargs)
         return self.G_to_F(r, _gr, q, dgr=_dgr, **kwargs)
@@ -508,9 +542,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`G_{Keen Version}(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`S(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`S(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         _gr, _dgr = self.converter.GK_to_G(r, gr, dgr=dgr, **kwargs)
         return self.G_to_S(r, _gr, q, dgr=_dgr, **kwargs)
@@ -525,9 +561,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`G_{Keen Version}(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`F(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`F(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         _gr, _dgr = self.converter.GK_to_G(r, gr, dgr=dgr, **kwargs)
         return self.G_to_FK(r, _gr, q, dgr=_dgr, **kwargs)
@@ -542,9 +580,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`G_{Keen Version}(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`\\frac{d \\sigma}{d \\Omega}(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`\\frac{d \\sigma}{d \\Omega}(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         _gr, _dgr = self.converter.GK_to_G(r, gr, dgr=dgr, **kwargs)
         return self.G_to_DCS(r, _gr, q, dgr=_dgr, **kwargs)
@@ -560,9 +600,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`g(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`Q[S(Q)-1]` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`Q[S(Q)-1]`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         _gr, _dgr = self.converter.g_to_G(r, gr, dgr=dgr, **kwargs)
         return self.G_to_F(r, _gr, q, dgr=_dgr, **kwargs)
@@ -577,9 +619,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`g(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`S(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`S(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         _gr, _dgr = self.converter.g_to_G(r, gr, dgr=dgr, **kwargs)
         return self.G_to_S(r, _gr, q, dgr=_dgr, **kwargs)
@@ -594,9 +638,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`g(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`F(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`F(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         _gr, _dgr = self.converter.g_to_G(r, gr, dgr=dgr, **kwargs)
         return self.G_to_FK(r, _gr, q, dgr=_dgr, **kwargs)
@@ -611,9 +657,11 @@ class Transformer:
         :type gr: numpy.array or list
         :param q: :math:`Q`-space vector
         :type q: numpy.array or list
+        :param dgr: :math:`g(r)` uncertainties
+        :type dgr: numpy.array or list
 
-        :return: :math:`Q` and :math:`\\frac{d \\sigma}{d \\Omega}(Q)` vector pair
-        :rtype: numpy.array pair
+        :return: :math:`Q`, :math:`\\frac{d \\sigma}{d \\Omega}(Q)`, and uncertainties
+        :rtype: numpy.array, numpy.array, numpy.array
         """
         _gr, _dgr = self.converter.g_to_G(r, gr, dgr=dgr, **kwargs)
         return self.G_to_DCS(r, _gr, q, dgr=_dgr, **kwargs)
