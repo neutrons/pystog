c02aaee841b1c4c929168b8569d1b6e1db482db5
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-01-06 14:19:09 -0500

    Fix lint errors in cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@ import pytest
 from pystog.cli import pystog_cli
 from pystog.stog import NoInputFilesException
 
+
 def test_pystog_cli_no_files_exception():
     with pytest.raises(NoInputFilesException):
         pystog_cli({"cat": "meow"})
-
