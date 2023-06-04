ed13c187c633bd98ffa35c28fe48955eaf2ff942
Yuanpeng Zhang <zyroc1990@gmail.com>
2023-06-04 18:44:51 -0400

    minor fix

--- pystog/__init__.py
+++ pystog/__init__.py
@@ -2,8 +2,9 @@ from pystog.stog import StoG
 from pystog.converter import Converter
 from pystog.transformer import Transformer
 from pystog.fourier_filter import FourierFilter
+from pystog.pre_proc import Pre_Proc
 
-__all__ = ['StoG', 'Converter', 'Transformer', 'FourierFilter', ]
+__all__ = ['StoG', 'Converter', 'Transformer', 'FourierFilter', 'Pre_Proc', ]
 
 from ._version import get_versions
 __version__ = get_versions()['version']
